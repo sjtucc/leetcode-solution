72287a53be9965bb42f64c3a208cf62bdb31e211
chen <cchen6666@126.com>
2016-06-28 23:11:08 +0800

    Longest Common Prefix

---
+++ leetcode_14.java
@@ -0,0 +1,50 @@
+package cn.cc.org;
+
+public class leetcode_14 {
+	public String longestCommonPrefix(String[] strs) {
+        if (strs == null) {
+            return null;
+        }
+
+        if (strs.length == 0) {
+            return "";
+        }
+
+        int min = Integer.MAX_VALUE;  // 记录最短的字符串的长度
+
+        // 找短字符串的长度
+        for (String str : strs) {
+
+            if (str == null) {
+                return null;
+            }
+
+            if (min > str.length()) {
+                min = str.length();
+            }
+        }
+
+        int i; // 记录最长前缀的字符数
+        boolean flag;
+        for (i = 0; i < min; i++) {
+            flag = true;
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[0].charAt(i) != strs[j].charAt(i)) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (!flag) {
+                break;
+            }
+        }
+
+//        if (i == 0) {
+//            return null;
+//        }
+
+        return strs[0].substring(0, i);
+
+    }
+}
