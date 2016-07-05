1a18cbed04c2d48282dac2a2eeccb3a95487ba49
chen <cchen6666@126.com>
2016-07-05 16:11:27 +0800

    Combination Sum

---
+++ leetcode_039.java
@@ -0,0 +1,31 @@
+package cn.cc.org;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class leetcode_39 {
+	   List<List<Integer>> result;  
+	    List<Integer> solu;  
+	    public List<List<Integer>> combinationSum(int[] candidates, int target) {  
+	        result = new ArrayList<>();  
+	        solu = new ArrayList<>();  
+	        Arrays.sort(candidates);  
+	        getCombination(candidates, target, 0, 0);  
+	        return result;  
+	    }  
+	    public void getCombination(int[] candidates, int target, int sum, int level){  
+	        if(sum>target) return;  
+	        if(sum==target){  
+	            result.add(new ArrayList<>(solu));  
+	            return;  
+	        }  
+	        for(int i=level;i<candidates.length;i++){  
+	            sum+=candidates[i];  
+	            solu.add(candidates[i]);  
+	            getCombination(candidates, target, sum, i);  
+	            solu.remove(solu.size()-1);  
+	            sum-=candidates[i];  
+	        }  
+	    }  
+}
