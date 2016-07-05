c990b370384fcf90baf4e8da220c82728b671798
chen <cchen6666@126.com>
2016-07-05 16:11:27 +0800

    Count and Say

---
+++ leetcode_038.java
@@ -0,0 +1,37 @@
+package cn.cc.org;
+
+public class leetcode_38 {
+	public String countAndSay(int n) {
+
+        if (n < 1) {
+            return null;
+        }
+
+        String result = "1";
+        for (int i = 2; i <=n ; i++) {
+            result = countAndSay(result);
+        }
+
+        return result;
+    }
+
+    public String countAndSay(String str) {
+        StringBuilder builder = new StringBuilder(128);
+
+        int count = 1;
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                count++;
+            } else {
+                builder.append(count);
+                builder.append(str.charAt(i - 1));
+                count = 1;
+            }
+        }
+
+        builder.append(count);
+        builder.append(str.charAt(str.length() - 1));
+        return builder.toString();
+    }
+
+}
