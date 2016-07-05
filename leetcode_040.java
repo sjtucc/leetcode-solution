663aabff584be5ab8bb80fd9c52ed67a74ca3d2b
chen <cchen6666@126.com>
2016-07-05 16:11:29 +0800

    Combination Sum II

---
+++ leetcode_040.java
@@ -0,0 +1,43 @@
+package cn.cc.org;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class leetcode_40 {
+	void dfs(int [] num, int start, int target, ArrayList<Integer> array, ArrayList<ArrayList<Integer>> result) {  
+        if(target==0) {  
+            result.add(new ArrayList<Integer>(array));  
+            return;  
+        }  
+          
+        if(start>=num.length||num[0]>target) {  
+            return;  
+        }  
+        int i = start;  
+        while(i<num.length) {  
+            if(num[i]<=target) {  
+                array.add(num[i]);  
+                dfs(num, i + 1, target - num[i], array, result);  
+                array.remove(array.size()-1);  
+                //跳过重复的元素  
+                while(i<(num.length-1)&&num[i]==num[i+1]) {  
+                    i++;  
+                }  
+            }  
+            i++;  
+        }  
+    }  
+      
+      
+    public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {  
+         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();  
+         ArrayList<Integer> array = new ArrayList<Integer>();  
+         if(num==null) {  
+             result.add(array);  
+             return result;  
+         }  
+         Arrays.sort(num);  
+         dfs(num,0, target,array,result);   
+         return result;  
+    }  
+}
