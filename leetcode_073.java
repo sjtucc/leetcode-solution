0aa0bd8851e3ab167d7cd5c4835a2f292cc37c40
chen <cchen6666@126.com>
2016-07-25 15:42:05 +0800

    Set Matrix Zeroes

---
+++ leetcode_073.java
@@ -0,0 +1,33 @@
+package cn.cc.org;
+
+public class leetcode_073 {
+	public int climbStairs(int n) {
+
+        int result = 0;
+
+        // 只有一阶
+        if (n == 1) {
+            result = 1;
+        }
+        // 只有两阶
+        else if (n == 2) {
+            result = 2;
+        }
+        // 楼梯阶数大于2
+        else if (n > 2) {
+            // 保存所有的解法
+            int[] ways = new int[n];
+
+            ways[0] = 1;
+            ways[1] = 2;
+
+            for (int i = 2; i < ways.length; i++) {
+                ways[i] = ways[i - 1] + ways[i - 2];
+            }
+
+            result = ways[ways.length - 1];
+        }
+
+        return result;
+    }
+}
